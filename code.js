6622b40f1a38291d9c11ea57e8950426c99dff20
IamLAM <ingeniero@luismonge.com.mx>
2019-04-06 01:53:00 -0700

    Using regular expression with and with out OR

--- code.js
+++ code.js
@@ -1,1 +1,11 @@
 //In this course you will learn about regular expressions
+
+let myString = "Good morning, my name is...!";
+let myRegex = /name/;
+let result = myRegex.test(myString);
+console.log(result);
+
+let myString2 = "Good morning, my name is Paola!";
+let myRegex2 = /Penelope|Paulina|Pilar|Petronila/; //using or for multiple options
+let result2 = myRegex2.test(myString2);
+console.log(result2);
