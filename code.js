4cbea30b3567d7b617cca12db3749ff8dc44ba6f
IamLAM <ingeniero@luismonge.com.mx>
2019-04-06 01:57:39 -0700

    ignoring cases with i

--- code.js
+++ code.js
@@ -9,3 +9,10 @@ let myString2 = "Good morning, my name is Paola!";
 let myRegex2 = /Penelope|Paulina|Pilar|Petronila/; //using or for multiple options
 let result2 = myRegex2.test(myString2);
 console.log(result2);
+
+//ignoring capitalize or variations
+
+let myString3 = "family";
+let fccRegex3 = /FAMILY/i; // Change this line
+let result3 = fccRegex3.test(myString3);
+console.log(result3);
